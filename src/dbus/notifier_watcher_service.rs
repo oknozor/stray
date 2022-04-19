78079c0ad75210ea0bcf23227acb125eca43d1ee
Paul Delafosse <paul.delafosse@protonmail.com>
2022-04-19 14:20:53 +0200

    fix: correctly remove icons using status notifier dbus address

--- src/dbus/notifier_watcher_service.rs
+++ src/dbus/notifier_watcher_service.rs
@@ -42,7 +42,7 @@ impl Watcher {
             let removed = self.registered_status_notifier_items.remove(&notifier);
             if removed {
                 self.sender
-                    .send(Message::Remove { address: notifier })
+                    .send(Message::Remove { address: notifier_address.to_string() })
                     .await
                     .unwrap();
             }
